a0e430a9c5ef59e6db9cf10855b68dca4d6b64c3
AbiDHussen <abidhussen351@gmail.com>
2024-07-04 17:37:27 +0600

    Initial Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Abid is a great boy.")),
+      body: Center(child: Text("Abid is a bad boy.")),
     );
   }
 }
