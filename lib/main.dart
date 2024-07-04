817a6ccf3d6a3730a35e293020c3eb99036f9edd
AbiDHussen <abidhussen351@gmail.com>
2024-07-04 17:23:39 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:untitled5/home_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeScreen(),);
+  }
+}
